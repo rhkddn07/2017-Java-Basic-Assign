54f7b4de61344ae26f4fd7c96a66a1928cd05020
rhkddn07 <rhkddn07@naver.com>
2017-03-21 15:44:19 +0900

    20131603 이광우 컴퓨터공학과2A
    숫자를 입력받아 그 수만큼 배열 합/평균구하는 프로그램

--- Average.java
+++ Average.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-
-public class Average { 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
-        Scanner input = new Scanner(System.in);  
-        int size = input.nextInt();    
-        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
-        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
-        
-        //프로그래밍 하세요.
-        
-        System.out.println("합: " + sum + ", 평균: " + sum/size);
-    }
+public class java20170321 {
+ public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     int SizeNum;
+     double scanNum;
+     double sum = 0;
+     double avg = 0;
+     System.out.println("입력할 최대 실수의 갯수를 입력하세요. >>");
+     SizeNum = scan.nextInt();
+     double[] Arr = new double[SizeNum];
+     System.out.println("중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");
+     System.out.println("실수 " +SizeNum+"개를 입력하세요.");
+     for (int i = 0; i<=SizeNum-1; i++) {
+         scanNum = scan.nextDouble();
+         Arr[i] = scanNum;
+         if(scanNum == 0) break;
+         sum += Arr[i];
+         avg = sum/(i+1);
+     }
+     System.out.println("합 : "+sum+ ", 평균  : " +avg);
+ }
 }
